edbfd4d6c13f48edc26431db924b9cc380ddbb45
Quillraven <quillraven@gmail.com>
2026-05-22 22:20:18 +0200

    adjust health values of enemies

--- core/src/main/kotlin/io/github/quillraven/flekstd/cfg/enemyCfg.kt
+++ core/src/main/kotlin/io/github/quillraven/flekstd/cfg/enemyCfg.kt
@@ -16,14 +16,14 @@ data class EnemyCfg(
     companion object {
         fun byEnemyKey(key: String): EnemyCfg = when (key) {
             "pawn" -> EnemyCfg(
-                health = { Health(base = 1f, regeneration = 0f) },
+                health = { Health(base = 3f, regeneration = 0f) },
                 speed = { Speed(3f) },
                 scale = 2.25f,
                 gdxAnimations = animationMapOf("pawn", AnimationType.RUN),
             )
 
             "lancer" -> EnemyCfg(
-                health = { Health(base = 10f, regeneration = 0.5f) },
+                health = { Health(base = 20f, regeneration = 0.5f) },
                 speed = { Speed(1.5f) },
                 scale = 4.5f,
                 gdxAnimations = animationMapOf("lancer", AnimationType.RUN),
