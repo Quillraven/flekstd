64310f709fea34dde11bb6a2dd3dbba87f7350d8
Quillraven <quillraven@gmail.com>
2026-05-27 21:54:29 +0200

    update facing of entities when they move left

--- core/src/main/kotlin/io/github/quillraven/flekstd/system/FollowPathSystem.kt
+++ core/src/main/kotlin/io/github/quillraven/flekstd/system/FollowPathSystem.kt
@@ -4,11 +4,12 @@ import com.github.quillraven.fleks.Entity
 import com.github.quillraven.fleks.IteratingSystem
 import com.github.quillraven.fleks.World.Companion.family
 import io.github.quillraven.flekstd.component.FollowPath
+import io.github.quillraven.flekstd.component.Render
 import io.github.quillraven.flekstd.component.Speed
 import io.github.quillraven.flekstd.component.Transform
 
 class FollowPathSystem : IteratingSystem(
-    family = family { all(FollowPath, Transform, Speed) }
+    family = family { all(FollowPath, Transform, Speed, Render) }
 ) {
     override fun onTickEntity(entity: Entity) {
         val followPathCmp = entity[FollowPath]
@@ -17,6 +18,10 @@ class FollowPathSystem : IteratingSystem(
 
         val from = followPathCmp.path[followPathCmp.currentPathIndex - 1]
         val to = followPathCmp.path[followPathCmp.currentPathIndex]
+
+        if (followPathCmp.alpha == 0f) {
+            entity[Render].flipX = to.x < from.x
+        }
 
         followPathCmp.alpha += deltaTime * speed / from.dst(to)
         position.set(from).lerp(to, followPathCmp.alpha.coerceAtMost(1f))
